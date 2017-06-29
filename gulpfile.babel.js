da682f08e2a7651ce2b9141f002a024d108fae8f
chartzoge <chartzoge@gmail.com>
2017-06-29 14:29:45 -0400

    it all works with nyc :shrug:

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,11 +1,15 @@
 "use strict";
 
 const gulp = require("gulp");
+const babel = require("gulp-babel");
 const del = require("del");
 const runSequence = require("run-sequence");
 const isparta = require("isparta");
 const mocha = require("gulp-mocha");
+const injectModules = require("gulp-inject-modules");
 const istanbul = require("gulp-istanbul");
+
+const debug = require("gulp-debug");
 
 const SOURCE_CODE_PATH = "./server/**/*.js";
 
@@ -15,6 +19,7 @@ gulp.task("clean", function () {
 
 gulp.task("coverage", function () {
     return  gulp.src([SOURCE_CODE_PATH])
+    .pipe(debug({ title: "debug" }))
     .pipe(istanbul({
         instrumenter: isparta.Instrumenter,
         includeUntested: true
@@ -24,15 +29,17 @@ gulp.task("coverage", function () {
 
 gulp.task("test", function () {
     return gulp.src("test/**/*.js", { read: false })
-    .pipe(mocha());
+    .pipe(mocha())
+    .pipe(istanbul.writeReports());
 });
 
 gulp.task("report", function () {
     gulp.src([SOURCE_CODE_PATH], { read: false })
+    .pipe(debug({ title: "debug" }))
     .pipe(istanbul.writeReports());
 });
 
 gulp.task("default", function (done) {
-    runSequence("clean", "coverage", "test", "report", done);
+    runSequence("clean", "coverage", "test", done);
 });
 
---
+++ server/not_tested.js
@@ -0,0 +1,11 @@
+"use strict";
+
+module.exports = class Foo {
+    constructor () {
+        this.blah = "blah";
+    }
+
+    do () {
+        return "blah";
+    }
+}
